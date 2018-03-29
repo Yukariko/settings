b0b56f301b2aeed66e5b3e6622c0b97f9ae8a443
kangmin <kangmin@rtst.co.kr>
2018-03-29 15:58:37 +0900

    add alias

--- install.sh
+++ install.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+# vim settings
 ln -s .vimrc ~/.vimrc
+
+# tmux settings
 ln -s .tmux.conf ~/.tmux.conf
+
+# alias settings
+ln -s .alias ~/.bashrc.kangmin
+echo "source ~/.bashrc.kangmin" >> ~/.bashrc
