0fab5b64e7af9870fe95e9d084510381a2790507
kangmin <kangmin@rtst.co.kr>
2018-03-29 17:46:09 +0900

    fix ln bug

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # vim settings
-ln -s .vimrc ~/.vimrc
+ln -sfn `pwd`/.vimrc ~/.vimrc
 
 # tmux settings
-ln -s .tmux.conf ~/.tmux.conf
+ln -sfn `pwd`/.tmux.conf ~/.tmux.conf
 
 # alias settings
-ln -s .alias ~/.bashrc.kangmin
+ln -sfn `pwd`/.alias ~/.bashrc.kangmin
 echo "source ~/.bashrc.kangmin" >> ~/.bashrc
