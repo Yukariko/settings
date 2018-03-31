700e12a17b45f89b3a405ef1768ffdf66e312da4
kangmin <kangmin@rtst.co.kr>
2018-03-31 22:57:33 +0900

    add zsh settings

--- install.sh
+++ install.sh
@@ -9,3 +9,9 @@ ln -sfn `pwd`/.tmux.conf ~/.tmux.conf
 # alias settings
 ln -sfn `pwd`/.alias ~/.bashrc.kangmin
 echo "source ~/.bashrc.kangmin" >> ~/.bashrc
+
+# zsh settings
+curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
+echo "ZSH_THEME=pygmalion" >> ~/.oh-my-zsh/custom/example.zsh
+ln -sfn `pwd`/.alias ~/.oh-my-zsh/custopm/alias.zsh
+
