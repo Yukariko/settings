664cdd7ddf4498d78591fd7245eb6e31b591940a
kangmin <kangmin@rtst.co.kr>
2018-04-04 18:47:54 +0900

    add fish shell

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # vim settings
-ln -sfn `pwd`/.vimrc ~/.vimrc
+ln -sfn `pwd`/vim/.vimrc ~/.vimrc
 vim -c PlugInstall -c q -c q
 
 # tmux settings
-ln -sfn `pwd`/.tmux.conf ~/.tmux.conf
+ln -sfn `pwd`/tmux/.tmux.conf ~/.tmux.conf
 
 # alias settings
 ln -sfn `pwd`/.alias ~/.bashrc.kangmin
@@ -16,3 +16,9 @@ curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | 
 echo "ZSH_THEME=pygmalion" >> ~/.oh-my-zsh/custom/example.zsh
 ln -sfn `pwd`/.alias ~/.oh-my-zsh/custom/alias.zsh
 
+# fish shell settings
+brew install fish
+curl -L https://get.oh-my.fish | fish
+ln -sfn `pwd`/fish/fish_prompt.fish  ~/.local/share/omf/themes/default/fish_prompt.fish
+ln -sfn `pwd`/fish/key_bindings.fish ~/.config/omf/key_bindings.fish
+omf reload
